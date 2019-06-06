de87c6fbd25ab52c0eab0d8c812f7209d3354644
ai-cuicui <ai_hasana@163.com>
2019-06-06 13:46:29 +0800

    Closes #138, 暂时关闭左边编辑，右边预览以及全屏功能

--- src/router/AppSettings/editorDesc/index.js
+++ src/router/AppSettings/editorDesc/index.js
@@ -17,8 +17,26 @@ class EditorDesc extends Component {
                 id="your-custom-id"
                 value={this.props.store.codeStore.settingData.description}
                 options={{
-                    spellChecker: false
+                    spellChecker: false,
+                    toolbar: [
+                        'bold',
+                        'italic',
+                        'heading',
+                        '|',
+                        'quote',
+                        'code',
+                        'table',
+                        'horizontal-rule',
+                        'unordered-list',
+                        'ordered-list',
+                        '|',
+                        'link',
+                        'image',
+                        '|',
+                        'preview'
+                    ]
                 }}
+
                 onChange={this.handleChange}
             />
         )
