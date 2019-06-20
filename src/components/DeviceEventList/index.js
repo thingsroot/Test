b2a3525251fe1c087f4b31b9da49106f11d96644
ai-cuicui <ai_hasana@163.com>
2019-06-20 15:44:04 +0800

    样式

--- src/components/DeviceEventList/index.js
+++ src/components/DeviceEventList/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Table, Input, Select, Button, message, Icon} from 'antd'
+import {Table, Input, Select, Button, message, Icon } from 'antd'
 import './style.scss'
 import http from '../../utils/Server';
 import axios from 'axios';
@@ -594,6 +594,15 @@ class DeviceEventList extends Component {
                         />
                     </div>
                 </div>
+                <div style={{padding: '10px 0'}}>
+                    {'全部消息' + messageCount + '条，列表中未确认消息' + unconfirmed + '条，'}
+                    <span
+                        style={{color: 'blue', cursor: 'pointer'}}
+                        onClick={this.toggleMessage}
+                    >
+                                    {showUnDisposed ? '查看所有' : '查看未确认'}
+                                </span>
+                </div>
                 <Table
                     rowSelection={rowSelection}
                     columns={columns}
@@ -609,19 +618,6 @@ class DeviceEventList extends Component {
                                 data={record}
                                 onConfirm={this.confMessage}
                             />
-                        )
-                    }}
-                    footer={() => {
-                        return (
-                            <div className="none">
-                            {'全部消息' + messageCount + '条，列表中未确认消息' + unconfirmed + '条，'}
-                            <span
-                                style={{color: 'blue', cursor: 'pointer'}}
-                                onClick={this.toggleMessage}
-                            >
-                                {showUnDisposed ? '查看所有' : '查看未确认'}
-                            </span>
-                        </div>
                         )
                     }}
                 />
