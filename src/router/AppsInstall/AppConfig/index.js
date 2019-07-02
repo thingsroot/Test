959d2916869f0dceedf19954440e33c41c3da485
viccom <Pa88worddong>
2019-07-02 21:59:28 +0800

    add Baud rate value

--- src/router/AppsInstall/AppConfig/index.js
+++ src/router/AppsInstall/AppConfig/index.js
@@ -68,7 +68,7 @@ function get_serial_childs (sn) {
                 'name': 'baudrate',
                 'desc': '波特率',
                 'type': 'dropdown',
-                'values': [4800, 9600, 115200, 19200],
+                'values': [1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200],
                 'default': 9600
             },
             {
@@ -110,7 +110,7 @@ function get_serial_childs (sn) {
             'name': 'baudrate',
             'desc': '波特率',
             'type': 'dropdown',
-            'values': [4800, 9600, 115200, 19200],
+            'values': [1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200],
             'default': 9600
         },
         {
