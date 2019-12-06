ca8d082f18196fd54a89e43f27dd31bbfe743bff
a980263316 <980263316@qq.com>
2019-12-06 17:41:35 +0800

    login page add help button

--- src/router/Login/index.js
+++ src/router/Login/index.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+import {Dropdown, Icon, Menu} from 'antd';
 import { isAuthenticated } from '../../utils/Session';
 import './login.scss'
 import Background from '../../assets/images/background.png';
@@ -27,6 +28,39 @@ class Login extends PureComponent {
         }
     }
     render () {
+        const menu1 = (
+            <Menu>
+                <Menu.Item
+                    key="16"
+                    style={{lineHeight: '30px'}}
+                    onClick={
+                        ()=>{
+                            window.open('http://help.cloud.thingsroot.com/quick_start/', '_blank')
+                        }
+                    }
+                >
+                    <Icon type="monitor" />
+                    <span>
+                        快速指南
+                    </span>
+                </Menu.Item>
+                <Menu.Item
+                    key="17"
+                    style={{lineHeight: '30px'}}
+                    onClick={
+                        ()=>{
+                            window.open('http://help.cloud.thingsroot.com/app_api_book/', '_blank')
+                        }
+                    }
+                >
+                    <Icon type="read" />
+                    <span>
+                        应用开发手册
+                    </span>
+                </Menu.Item>
+
+            </Menu>
+        )
         const { path } = this.props.match;
         return (
             <div className="login"
@@ -39,6 +73,56 @@ class Login extends PureComponent {
                         />
                         <span>{OEM.Title}</span>
                     </p>
+                    <div className="help_link">
+                        {
+                        OEM.Title === '冬笋云'
+                        ? <span>
+                            <Dropdown
+                                overlay={menu1}
+                                placement="bottomRight"
+                            >
+                            <span
+                                className="ant-dropdown-link"
+                                style={{padding: '10px', cursor: 'pointer'}}
+                            >
+                                <Icon
+                                    style={{padding: '0 4px', fontWeight: 800}}
+                                    type="question-circle"
+                                />帮助
+                            </span>
+                        </Dropdown>
+                        <span style={{padding: '0 5px'}}> </span>
+                        <a onClick={()=>{
+                            window.open('https://freeioe.org/', '_blank')
+                        }}
+                        >
+                            <Icon
+                                style={{padding: '0 4px', fontWeight: 800}}
+                                type="message"
+                            />
+
+                            讨论
+                        </a>
+                        <span style={{padding: '0 5px'}}> </span>
+
+                        <span style={{padding: '0 5px'}}> </span>
+                        <a onClick={()=>{
+                            window.open('https://wiki.freeioe.org/', '_blank')
+                        }}
+                        >
+                            <Icon
+                                style={{padding: '0 4px', fontWeight: 800}}
+                                type="book"
+                            />
+
+                            知识库
+                        </a>
+                        <span style={{padding: '0 5px'}}> </span>
+                        </span>
+
+                        : ''
+                    }
+                    </div>
                 </div>
                 <div className="main">
                     <div className="tabs">
