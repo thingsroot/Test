d45b436c3e24da1252c5f5ea38eb64ccc7583d7a
a980263316 <980263316@qq.com>
2020-01-13 18:09:14 +0800

    添加在线编辑模板功能，网关临时共享功能

---
+++ src/router/TemplateDetails/Wps/index.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from 'react';
+import {withRouter} from 'react-router-dom';
+import http from '../../../utils/Server';
+@withRouter
+class Wps extends PureComponent {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            iFrameHeight: '100%'
+        }
+    }
+    componentDidMount () {
+        const {version} = this.props;
+        http.get('/api/get_wps_url?conf=' + this.props.match.params.name + '&version=' + version + '&version_new=' + (version + 1)).then(res=>{
+            if (res.ok) {
+                this.setState({
+                    url: res.url
+                })
+            }
+        })
+    }
+    render () {
+        return (
+            <div
+                id="wps_wrap"
+            >
+                <iframe
+                    style={{width: '100%', height: this.state.iFrameHeight, overflow: 'visible'}}
+                    ref="iframe"
+                    src={this.state.url}
+                    width="100%"
+                    height={this.state.iFrameHeight}
+                    scrolling="no"
+                    frameBorder="0"
+                />
+                </div>
+        )
+    }
+}
+
+export default Wps
